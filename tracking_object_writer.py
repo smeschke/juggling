3e1c113f8a7e82d9a1737fb090cf6d1b0d17c0c6
smeschke <stephenmeschke@gmail.com>
2018-11-12 13:04:42 -0800

    Update tracking_object_writer.py
    made this file more simple, got rid of the video writer

--- tracking_object_writer.py
+++ tracking_object_writer.py
@@ -1,76 +1,71 @@
-import cv2
+import cv2, math
 import numpy as np
+import pandas as pd
+import scipy
+from scipy import signal
 
+#------------------Enter these parameters------------------
+# Path to source video - find here: https://drive.google.com/open?id=0B7QqDexrxSfwVXgwWlRUcDBiVFk
+source_path = '/home/stephen/Desktop/ss642_id_140.MP4'
+# Path to source data - find here: https://drive.google.com/open?id=0B7QqDexrxSfwVXgwWlRUcDBiVFk
+data_path = '/home/stephen/Desktop/ss642_id_140.csv'
+# Smoothing parameters
+window_length, polyorder = 15, 2
+# List of colors
+colors = (0,255,255),  (255,0,255),(255,255,0), (123,123,255), (123,255,123), (255,123,123), (255,0,0),(0,255,0)
+# Parameters for animation
+tail_length, tail_thickness = 15, .8
 
-vid_writer = cv2.VideoWriter('/home/stephen/Desktop/peter_every_out.avi',
-                             cv2.VideoWriter_fourcc('M','J','P','G'),
-                             60, (620,1000))
+# Read source video
+cap = cv2.VideoCapture(source_path)
 
+# Read data
+df = pd.read_csv(data_path)
+# Create list for smoothed data
+smoothed_data = []
+# Iterate through each column in the database
+for i in range(int(len(df.columns)/2)):
+    # Smooth the column
+    x = signal.savgol_filter(df[df.columns[i*2]], window_length, polyorder)
+    y = signal.savgol_filter(df[df.columns[i*2+1]], window_length, polyorder)
+    # Add the smoothed data to the smoothed_data list
+    smoothed_data.append(list(zip(x,y)))
 
-#read tracking data from file
-import csv
-positions1 = []
-with open('/home/stephen/Desktop/trick_5bHalfShower_id_85.csv', 'r') as csvfile:
-    data = csv.reader(csvfile)
-    for row in data:
-        try: x,y = int(row[0]), int(row[1])
-        except: x,y = 0,0
-        positions1.append((x,y))
-##positions2 = []
-##with open('/home/stephen/Desktop/2.csv', 'r') as csvfile:
-##    data = csv.reader(csvfile)
-##    for row in data:
-##        try: x,y = int(row[0]), int(row[1])
-##        except: x,y = 0,0
-##        positions2.append((x,y))
-##positions3 = []
-##with open('/home/stephen/Desktop/3.csv', 'r') as csvfile:
-##    data = csv.reader(csvfile)
-##    for row in data:
-##        try: x,y = int(row[0]), int(row[1])
-##        except: x,y = 0,0
-##        positions3.append((x,y))
-##positions4 = []
-##with open('/home/stephen/Desktop/4.csv', 'r') as csvfile:
-##    data = csv.reader(csvfile)
-##    for row in data:
-##        try: x,y = int(row[0]), int(row[1])
-##        except: x,y = 0,0
-##        positions4.append((x,y))
-
-
-#read source video
-cap = cv2.VideoCapture('/home/stephen/Desktop/IMG_1230.MOV')
-
-
-#start counting by frame number
+# Start counting by frame number
 frame_number = 0
+# Iterate through each frame of video
 while True:
     _, img = cap.read()
-    #_, img1 = cap.read()
-    #img = cv2.addWeighted(img, .5, img1, .5, 1)
-    #try: img = np.zeros_like(img)
-    try: d = img.shape
+    # Break out if the video is over
+    try: img.shape
     except: break
-    tail_length = 20
-    for i in range(tail_length):
+
+    # Iterate though each ball in the smoothed data
+    for ball in smoothed_data:
+
+        # Get the ball color
+        color = colors[smoothed_data.index(ball)]
         
-        cv2.circle(img, positions1[frame_number-i], tail_length-i, (255,0,0), -1)
-        #cv2.circle(img, positions2[frame_number], 20, (0,255,0), -1)
-        #cv2.circle(img, positions3[frame_number], 20, (0,0,255), -1)
-        #cv2.circle(img, positions4[frame_number], 10, (255,123,0), 5)
-        #cv2.line(img, positions1[frame_number], positions2[frame_number], (234,12,12), 2)
-        #cv2.line(img, positions3[frame_number], positions4[frame_number], (12,12,234), 2)
-        
+        # Iterate though each frame in tail length
+        for tail in range(tail_length):
+            # Get the ball position as tuple, so like: (x,y)
+            position = tuple(ball[frame_number-tail])
+            position = int(position[0]), int(position[1])
+            # Get the ball position from the previous frame
+            previous_position = tuple(ball[frame_number-(tail+1)])
+            previous_position = int(previous_position[0]), int(previous_position[1])
+            # Avoid looping from the front to the back of the list
+            if tail+1-frame_number<0 and int((tail_length-tail)*tail_thickness)>0:                
+                # Draw the line                
+                cv2.line(img, position, previous_position, color, int((tail_length-tail)*tail_thickness))
+
+    # Show the image and wait                
     cv2.imshow('image',img)
     k = cv2.waitKey(1)
     if k == 27: break
     
-
+    # Increment frame number
     frame_number += 1
-    vid_writer.write(img)
     
 cv2.destroyAllWindows()
-
-
-    
+cap.release()
