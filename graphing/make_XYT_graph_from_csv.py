73ca355c640f70efa896a9bc0fbbefb8364469a1
smeschke <stephenmeschke@gmail.com>
2018-11-14 16:27:08 -0800

    Update make_XYT_graph_from_csv.py

--- graphing/make_XYT_graph_from_csv.py
+++ graphing/make_XYT_graph_from_csv.py
@@ -8,45 +8,57 @@ from scipy import signal
 distances = []
 
 # Get pose data from the spreadsheet
-df = pd.read_csv('/home/stephen/Desktop/663_tracking.csv')
+source_path = '/home/stephen/Desktop/source_data/ss77772_id_121.csv'
+df = pd.read_csv(source_path)
+
+# Define the image scale
+img_scale = 10
+buffer = 200
 
 # Smooth it out (column by column)
-smoothed_data = []
-window_length, exponent = 23, 2
+window_length, polyorder = 17, 2
 for i in range(int(len(df.columns)/2)):
-    df[str(i*2)] = signal.savgol_filter(df[str(i*2)], window_length, exponent)
-    df[str(i*2+1)] = signal.savgol_filter(df[str(i*2+1)], window_length, exponent)
+    df[df.columns[i*2]] = signal.savgol_filter(df[df.columns[i*2]], window_length, polyorder) * img_scale
+    df[df.columns[i*2+1]] = signal.savgol_filter(df[df.columns[i*2+1]], window_length, polyorder) * img_scale
 
-#list of colors
-colors = (0,255,255), (0,255,255), (255,0,255), (255,0,255),(0,255,0), (255,0,255), (0,255,255), (255,255,0), (255,0,0),(0,255,0)
+# List of colors
+colors = (0,255,255), (255,0,255),(255,255,0), (123,123,255), (123,255,123), (255,123,123), (255,0,0),(0,255,0),(0,0,255)
 
+# Get the max height of the data
+max_height = int(max(list(df[df.columns].max())))
+# Get the minimum height
+min_height = int(min(list(df[df.columns].min())))
+# Calculate image height]
+img_height = (max_height-min_height) + buffer*2
+# Get the length of the data
+length = len(df[df.columns[0]].values)*img_scale
 # Create image to draw on
-# Alter these values, depending on the height of the data, and the number of frames
-img = np.zeros((800,5000,3), np.uint8)
+img = np.zeros((img_height,length,3), np.uint8)
+img[:,:,:] = 67,67,67
 
-# Start with the first frame
-p_values = df.values[0]
-frame = 0
+# Graph the data
+idx = 0
+for i in range(int(len(df.columns)/2)):
+    color = colors[idx]
+    y = df[df.columns[i*2+1]]
+    x = df[df.columns[i*2]]
+    for frame in range(len(y.values)-1):
+        a = frame*img_scale, int(y[frame]) + buffer - min_height
+        b = (frame+1)*img_scale, int(y[frame+1]) + buffer - min_height
+        cv2.line(img, a,b, color, img_scale*2)
+    idx += 1   
 
-# Iterate though each frame
-for values in df.values:
-    frame_increment  = 4
-    frame += frame_increment
-    idx = 0
-    for value, p_value in zip(values, p_values):
-        color = colors[int(idx)]
-        idx += .5
-        # Draw only the y values (this is a YT graph)
-        if list(values).index(value)%2!=0:
-            cv2.line(img, (frame-frame_increment, int(p_value)), (frame, int(value)), color, 6)
-    p_values = values
+# Blur and resize the image
+img = cv2.blur(img, (int(img_scale), int(img_scale)))
+h,w,_ = img.shape
+img = cv2.resize(img, (int(w/img_scale), int(h/img_scale)))
+# Write the name of the trick
+org, font, scale, color, thick = (12,20), cv2.FONT_HERSHEY_SIMPLEX, .75, (234,234,234), 1
+cv2.putText(img, "Y-T graph for " + source_path.split('/')[-1][:-4], org, font, scale, color, thick, cv2.LINE_AA)
 
-# Crop the image so that it looks nice
-img = img[0:876, :]
 cv2.imshow('image',img)
 cv2.imwrite('/home/stephen/Desktop/graph.png', img)
-k = cv2.waitKey(0)
-    
+k = cv2.waitKey(0)    
 cv2.destroyAllWindows()
 
 
