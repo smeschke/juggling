1932a09ec7b9f38949a30a962943ceb6d690884f
smeschke <stephenmeschke@gmail.com>
2019-01-13 10:56:17 -0800

    Update colorspaces_tracking.py

--- tracking/colorspaces_tracking.py
+++ tracking/colorspaces_tracking.py
@@ -1,20 +1,23 @@
-import cv2
-import numpy as np
+# Import libraries
+import cv2, numpy as np
 
-#h,s,v range of the object to be tracked
-#h,s,v,h1,s1,v1 = 27,0,0,82,190,255 #GREEN
-#h,s,v,h1,s1,v1 = 168,154,99,177,255,255 #RED
+# H,S,V range of the object to be tracked
+
+h,s,v,h1,s1,v1 = 0,184,128,20,255,255 #RED
+
+# Find these values using hsv_color_picker.py
 #h,s,v,h1,s1,v1 = 156,74,76,166,255,255 #pink
-threshold_value = 0
+#h,s,v,h1,s1,v1 = 27,0,0,82,190,255 #GREEN
 
-output_path = '/home/stephen/Desktop/423_pink.csv'
+# Define data's output path
+output_path = '/home/stephen/Desktop/red_ball.csv'
 
-cap = cv2.VideoCapture('/home/stephen/Desktop/423.avi')
+# Define the source path
+cap = cv2.VideoCapture('/home/stephen/Desktop/source_vids/ss3_id_16.MP4')
 
-
-#takes an image, and a lower and upper bound
-#returns only the parts of the image in bounds
-def only_color(frame, (b,r,g,b1,r1,g1)):
+# Takes image and color, returns parts of image that are that color
+def only_color(frame, hsv_range):
+    (b,r,g,b1,r1,g1) = hsv_range
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     # define range of blue color in HSV
@@ -35,7 +38,7 @@ def largest_contour(contours):
 #takes an image and the threshold value returns the contours
 def get_contours(im, threshold_value):
     imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-    _ ,thresh = cv2.threshold(imgray,threshold_value,255,0)
+    _ ,thresh = cv2.threshold(imgray,0,255,0)
     _, contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     return contours
 
@@ -48,32 +51,45 @@ def contour_center(c):
     except: center = 0,0
     return center
 
-frame_number = 0
-positions = []
-for i in range(1000000): positions.append((0,0))
-#main loop of the program
-while True:
-    #read image from the video
-    _, img = cap.read()
+# Create list to save data
+frame_number, positions = 0, []
 
+# Iterate though each frame of video
+while True:
+    
+    # Read image from the video
+    _, img = cap.read()
+    
+
+    # Chech if the video is over
     try: l = img.shape
     except: break
-    #extract only the flesh tones
+
+    img_copy = img.copy()
+    
+    # Segment the image by color
     img, mask = only_color(img, (h,s,v,h1,s1,v1))
-    #find the contours in the image
-    contours = get_contours(img, threshold_value)
-    #if there are contours found in the image:
+
+    # Find the contours in the image
+    contours = get_contours(img, 0)
+
+    # If there are contours found in the image:
     if len(contours)>0:
         try:
-            #sort the contours by area
+            # Sort the contours by area
             c = max(contours, key=cv2.contourArea)
-            img = cv2.drawContours(img, c ,-1, (0,0,255), 14)
-            positions[frame_number] = contour_center(c)
-        except: pass        
+            # Draw the contours on the image
+            img = cv2.drawContours(img_copy, c ,-1, (0,0,255), 14)
+            # Add the data from the contour to the list
+            positions.append(contour_center(c))
+        except: pass
+
+    # If no data point got added, add another one
+    if len(positions) < frame_number: positions.append((0,0))
     frame_number += 1
     #show the image and wait
-    #cv2.imshow('img', img)
-    cv2.imshow('img', cv2.resize(img, (480,700)))
+    cv2.imshow('img', img_copy)
+    #cv2.imshow('img', cv2.resize(img, (480,700)))
     k=cv2.waitKey(1)
     if k==27: break
     
@@ -84,7 +100,7 @@ cv2.destroyAllWindows()
 #remove unused parts of the list
 positions = positions[:frame_number]
 
-print 'finished tracking'
+print('finished tracking')
 #write data
 import csv
 with open(output_path, 'w') as csvfile:
@@ -95,5 +111,4 @@ with open(output_path, 'w') as csvfile:
         x, y = position[0], position[1]
         writer.writerow({'x_position': x, 'y_position': y})
 
-print 'finished writing data'
-print output_path
+print('finished writing data')
