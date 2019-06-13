b4b1bc126b0772c83bcb87b2694daf1c116b21b3
Stephen Meschke <stephenmeschke@gmail.com>
2019-06-13 14:17:15 -0700

    Update bright_object_tracker.py
    Changed the find contours function so that it will work with opencv v. 4+

--- bright_object_tracker.py
+++ bright_object_tracker.py
@@ -19,7 +19,7 @@ while True:
     # Show the threshold image
     cv2.imshow('thresh', thresh)
     # Find the contours in the thresholded image
-    _, contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     # Find the contour centers
     positions = []
     # Iterate though the contours
