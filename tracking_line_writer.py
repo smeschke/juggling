496c7fa66976b15c4d73fe7385e13142b7fc5ade
Stephen Meschke <stephenmeschke@gmail.com>
2019-06-13 14:42:13 -0700

    Update tracking_line_writer.py
    Got rid of junk

--- tracking_line_writer.py
+++ tracking_line_writer.py
@@ -6,28 +6,18 @@ from scipy import signal
 
 #------------------Enter these parameters------------------
 # Path to source video - find here: https://drive.google.com/open?id=0B7QqDexrxSfwVXgwWlRUcDBiVFk
-path = 'ss71_id_162'
-source_path = '/home/stephen/Desktop/source_vids/' + path + '.MP4'
-#source_path = '/home/stephen/Desktop/' + path + '.avi'
-
+source_path = '/home/stephen/Desktop/ss5_id_321.MP4'
 # Path to source data - find here: https://drive.google.com/open?id=0B7QqDexrxSfwVXgwWlRUcDBiVFk
-#data_path = '/home/stephen/Desktop/source_data/' + path  + '.csv'
-data_path = '/home/stephen/Desktop/source_data/' + path  + '.csv'
+data_path = '/home/stephen/Desktop/ss5_id_321.csv'
 # Smoothing parameters
 window_length, polyorder = 15, 2
 # List of colors
 colors = (255,0,255),(255,255,0),(0,255,255),(0,0,255),(255,0,0), (0,255,0), (255,123,0),(0,255,123)
-#colors = (255,0,0),(0,0,255),(0,75,255),(0,156,255),(0,255,255), (0,255,0), (255,123,0),(0,255,123)
-#colors = (1,56,255),(234,234,234),(1,56,255),(234,234,234),(1,56,255),(234,234,234),(1,56,255),(234,234,234),(1,56,255),(234,234,234),(1,56,255)
-#colors = (1,56,255),(1,56,255),(45,45,45),(45,45,45),(1,56,255),(45,45,45),(1,56,255),(45,45,45),(1,56,255)
 
-# Parameters for animation
-hw = 720
-#hw = 780
-#hw = 848
+# Create video writer
 vid_writer = cv2.VideoWriter('/home/stephen/Desktop/test.avi',
                              cv2.VideoWriter_fourcc('M','J','P','G'),
-                             60, (hw,hw))
+                             60, (480,848))
 
 # Read source video
 cap = cv2.VideoCapture(source_path)
@@ -57,9 +47,6 @@ while True:
     try: img.shape
     except: break
 
-    alpha = np.zeros_like(img)
-    foreground = np.zeros_like(img)
-
     # Iterate though each ball in the smoothed data
     p = []
     for ball in smoothed_data:
@@ -74,42 +61,9 @@ while True:
         a = int(a[0]), int(a[1])
         b = int(b[0]), int(b[1])
         cv2.line(img, a, b, colors[i], tt)
-            
-    background= img.copy()
-
-    foreground = foreground.astype(float)
-    background = background.astype(float)
-
-    aalpha = alpha.copy()
-
-    
-    # Normalize the alpha mask to keep intensity between 0 and 1
-    alpha = alpha.astype(float)/255
-     
-    # Multiply the foreground with the alpha matte
-    foreground = cv2.multiply(alpha, foreground)
-     
-    # Multiply the background with ( 1 - alpha )
-    background = cv2.multiply(1.0 - alpha, background)
-     
-    # Add the masked foreground and background.
-    img = cv2.add(foreground, background)
-    img = img/255
-    img = np.array(img*255, np.uint8)
-
-    # mat to a square aspect
-    bg = np.zeros((848,848,3), np.uint8)
-    bg[:,:,:] = 123,123,123
-    bg[:,180:180+480] = img
-    img = bg
-
-    if hw == 780: img = img[848-780:848, 0+30:30+780] # for 780
-    if hw == 720: img = img[848-720:848, 0+50:50+720] #for 720x
-    
+        
     # Show the image and wait                
-    cv2.imshow('image',img)
-    #cv2.imshow('alpha',aalpha)    
-    
+    cv2.imshow('image',img)    
     vid_writer.write(img)
     k = cv2.waitKey(1)
     if k == 27: break
