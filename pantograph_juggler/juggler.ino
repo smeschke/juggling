41c8bbd18ff0e0115531c3224c3f5272aab0b68c
Stephen Meschke <stephenmeschke@gmail.com>
2019-09-25 09:32:22 -0700

    Update juggler.ino
    Changed the drivers to 4x microstepping. There are now 800 steps per revolution instead of 200. Also started the second motor when the first motor is only 3/4 the way through it's stoke.

--- pantograph_juggler/juggler.ino
+++ pantograph_juggler/juggler.ino
@@ -6,20 +6,20 @@ int side = 0;
 
 void setup()
 {  
-  stepper1.setMaxSpeed(380);
-  stepper1.setAcceleration(2345);  
-  stepper2.setMaxSpeed(380);
-  stepper2.setAcceleration(2345);  
+  stepper1.setMaxSpeed(2200);
+  stepper1.setAcceleration(4000);  
+  stepper2.setMaxSpeed(2200);
+  stepper2.setAcceleration(4000);
 }
 void loop()
 {
-    if (abs(stepper2.distanceToGo()) == 0 and abs(stepper1.distanceToGo()) == 0 and side == 0){
-      stepper1.moveTo(200+stepper1.currentPosition());
+    if (abs(stepper2.distanceToGo()) < 200 and abs(stepper1.distanceToGo()) == 0 and side == 0){
+      stepper1.moveTo(800+stepper1.currentPosition());
       side = 1;
     }
       
-    if (abs(stepper2.distanceToGo()) == 0 and abs(stepper1.distanceToGo()) == 0 and side == 1){
-      stepper2.moveTo(-200+stepper2.currentPosition());
+    if (abs(stepper2.distanceToGo()) == 0 and abs(stepper1.distanceToGo()) < 200 and side == 1){
+      stepper2.moveTo(-800+stepper2.currentPosition());
       side = 0;
     }
     
