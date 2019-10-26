b42e331ec0973a5576e127e5bb073a14045b84c3
Stephen Meschke <stephenmeschke@gmail.com>
2019-10-25 18:55:42 -0700

    Update toss_juggler.ino

--- pantograph_juggler/toss_juggler.ino
+++ pantograph_juggler/toss_juggler.ino
@@ -4,10 +4,12 @@ AccelStepper stepper2(AccelStepper::DRIVER, 4,3);
 
 int start = 0;
 int side = 0;
-int max_speed = 12000;
-int max_acceleration = 13200;
+int max_speed = 1800000;
+int max_acceleration = 16000;
 int starting_rotation = 200;
 int stepover = 250;
+int num_throws = 30;
+int idx = 0;
 void setup()
 {  
   stepper1.setMaxSpeed(max_speed);
@@ -15,7 +17,7 @@ void setup()
   stepper2.setMaxSpeed(max_speed);
   stepper2.setAcceleration(max_acceleration);
   stepper1.moveTo(starting_rotation);
-  stepper2.moveTo(-starting_rotation);  
+  stepper2.moveTo(starting_rotation);  
   }
   
 void loop()
@@ -28,134 +30,52 @@ void loop()
         stepper1.run();
       }
       start = 1;
-      delay(5500);
+      delay(3000);
+      
     }
 
     
     if (abs(stepper1.distanceToGo()) == 0 
           and abs(stepper2.distanceToGo()) < stepover 
-          and side == 0){
+          and side == 0
+          and idx < num_throws){
       stepper1.moveTo(-800+stepper1.currentPosition());
       side = 1;
-      //delay(500);
-    }
       
-    if (abs(stepper2.distanceToGo()) == 0
-          and abs(stepper1.distanceToGo()) < stepover
-          and side == 1){
-      stepper2.moveTo(800+stepper2.currentPosition());
-      side = 2;
-      //delay(500);
-    }
-    
-    if (abs(stepper1.distanceToGo()) == 0 
-          and abs(stepper2.distanceToGo()) < stepover 
-          and side == 2){
-      stepper1.moveTo(-800+stepper1.currentPosition());
-      side = 3;
-      //delay(500);
-    }
       
+    }
     if (abs(stepper2.distanceToGo()) == 0
-          and abs(stepper1.distanceToGo()) < stepover
-          and side == 3){
-      stepper2.moveTo(800+stepper2.currentPosition());
-      side = 4;
-      //delay(500);
-    }
-    if (abs(stepper1.distanceToGo()) == 0 
-          and abs(stepper2.distanceToGo()) < stepover 
-          and side == 4){
-      stepper1.moveTo(-800+stepper1.currentPosition());
-      side = 5;
-      //delay(500);
-    }
-
-    
-    if (abs(stepper2.distanceToGo()) == 0
-          and abs(stepper1.distanceToGo()) < stepover
-          and side == 5){
-      stepper2.moveTo(800+stepper2.currentPosition());
-      side = 6;
-      //delay(500);
-    }
-
-    if (abs(stepper1.distanceToGo()) == 0 
-          and abs(stepper2.distanceToGo()) < stepover 
-          and side == 6){
-      stepper1.moveTo(-800+stepper1.currentPosition());
-      side = 7;
-      //delay(500);
-    }
-
-    
-    if (abs(stepper2.distanceToGo()) == 0
-          and abs(stepper1.distanceToGo()) < stepover
-          and side == 7){
-      stepper2.moveTo(800+stepper2.currentPosition());
-      side = 8;
-      //delay(500);
-    }
-
-    if (abs(stepper1.distanceToGo()) == 0 
-          and abs(stepper2.distanceToGo()) < stepover 
-          and side == 8){
-      stepper1.moveTo(-800+stepper1.currentPosition());
-      side = 9;
-      //delay(500);
-    }
-
-    
-    if (abs(stepper2.distanceToGo()) == 0
-          and abs(stepper1.distanceToGo()) < stepover
-          and side == 9){
-      stepper2.moveTo(800+stepper2.currentPosition());
-      side = 10;
-      //delay(500);
-    }
-    
-    if (abs(stepper1.distanceToGo()) == 0 
-          and abs(stepper2.distanceToGo()) < stepover 
-          and side == 10){
-      stepper1.moveTo(-800+stepper1.currentPosition());
-      side = 11;
-      //delay(500);
-    }
-
-    
-    if (abs(stepper2.distanceToGo()) == 0
-          and abs(stepper1.distanceToGo()) < stepover
-          and side == 11){
-      stepper2.moveTo(800+stepper2.currentPosition());
-      side = 12;
-      //delay(500);
-    }
-    
-    if (abs(stepper1.distanceToGo()) == 0 
-          and abs(stepper2.distanceToGo()) < stepover 
-          and side == 12){
-      stepper1.moveTo(-800+stepper1.currentPosition());
-      side = 13;
-      //delay(500);
-    }
-
-    
-    if (abs(stepper2.distanceToGo()) == 0
-          and abs(stepper1.distanceToGo()) < stepover
-          and side == 13){
-      stepper2.moveTo(800+stepper2.currentPosition());
-      side = 14;
-      //delay(500);
-    }
-
-    if (side == 14 and abs(stepper1.distanceToGo()) == 0 and abs(stepper2.distanceToGo()) == 0){
+          and abs(stepper1.distanceToGo()) < stepover 
+          and side == 1
+          and idx < num_throws){
+      stepper2.moveTo(-800+stepper2.currentPosition());
       side = 0;
-      delay(5500);
+      idx += 1;
     }
     
     if (start == 1){
       stepper2.run();    
       stepper1.run();
     }
+
+    if (idx == num_throws and
+    abs(stepper1.distanceToGo()) == 0 
+          and abs(stepper2.distanceToGo()) ==0){
+            delay(5000);
+            idx = 0;
+          }
+    
+    
+//    if (idx == num_throws){
+//        stepper1.moveTo(stepper1.currentPosition()-starting_rotation);
+//        stepper2.moveTo(stepper2.currentPosition()-starting_rotation);  
+//        delay(1000);
+//        while (abs(stepper1.distanceToGo()) != 0 
+//              and abs(stepper2.distanceToGo()) != 0){
+//          stepper2.run();
+//          stepper1.run();
+//        }
+//      idx  = 15;
+//    }
     
 }
