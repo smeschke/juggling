faad9c67078ab09026b907560f3817c6d2857d6b
Stephen Meschke <stephenmeschke@gmail.com>
2021-02-28 15:17:33 -0800

    Update toss_juggler.ino
    removed the led pins

--- pantograph_juggler/toss_juggler.ino
+++ pantograph_juggler/toss_juggler.ino
@@ -4,66 +4,56 @@ AccelStepper stepper2(AccelStepper::DRIVER, 4, 3);
 
 int start = 0;
 int side = 0;
-int max_speed = 6000;
-int max_acceleration = 12000;
+int max_speed = 5200;
+int max_acceleration = 11000;
 int starting_rotation = 200;
-int stepover = 340;
-int ledPinR = 7;
-int ledPinL = 6;
+int stepover = 280;
 int step_distance = 800;
 
 void setup()
 {
+  //sdfsd
   stepper1.setMaxSpeed(max_speed);
-  stepper1.setAcceleration(max_acceleration);
   stepper2.setMaxSpeed(max_speed);
-  stepper2.setAcceleration(max_acceleration);
-  stepper1.moveTo(starting_rotation);
-  stepper2.moveTo(starting_rotation);
-  pinMode(ledPinR, OUTPUT);
-  pinMode(ledPinL, OUTPUT);
+  stepper1.setAcceleration(1234);
+  stepper2.setAcceleration(1234);
+  stepper1.moveTo(600);
+  stepper2.moveTo(-200);
 }
 
 void loop()
 {
-  
+
   if (start == 0) {
-    delay(2000);
-    while (abs(stepper1.distanceToGo()) != 0
-           and abs(stepper2.distanceToGo()) != 0) {
-      stepper2.run();
+    while (abs(stepper1.distanceToGo()) != 0) {
       stepper1.run();
     }
-    start = 1;
-    digitalWrite(ledPinR, HIGH); digitalWrite(ledPinL, HIGH);
-    for (int i = 0; i <= 15; i++){
-      delay(75); digitalWrite(ledPinL, LOW); digitalWrite(ledPinR, LOW);
-      delay(75); digitalWrite(ledPinL, HIGH); digitalWrite(ledPinR, HIGH);
+    while (abs(stepper2.distanceToGo()) != 0) {
+      stepper2.run();
     }
+    start = 1;
+    stepper1.setAcceleration(max_acceleration);
+    stepper2.setAcceleration(max_acceleration);
+    delay(500);
   }
-  
+
+
   if (abs(stepper1.distanceToGo()) == 0
       and abs(stepper2.distanceToGo()) < stepover
       and side == 0) {
-    //delay(2500);
     stepper1.moveTo(step_distance + stepper1.currentPosition());
     side = 1;
-    digitalWrite(ledPinL, HIGH);
   }
-  
+
   if (abs(stepper2.distanceToGo()) == 0
       and abs(stepper1.distanceToGo()) < stepover
       and side == 1) {
-    //delay(2500);
-    stepper2.moveTo(-step_distance + stepper2.currentPosition());
+    stepper2.moveTo(step_distance + stepper2.currentPosition());
     side = 0;
-    digitalWrite(ledPinR, HIGH);
   }
-  
+
   if (start == 1) {
     stepper2.run();
     stepper1.run();
   }
-  if (stepper1.distanceToGo()==0){ digitalWrite(ledPinL, LOW); }
-  if (stepper2.distanceToGo()==0){ digitalWrite(ledPinR, LOW); }
 }
